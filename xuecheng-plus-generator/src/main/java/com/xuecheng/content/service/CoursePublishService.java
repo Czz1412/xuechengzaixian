9fe0080f267fcbaa2b91a28c2ca4b17df08d3126
Czz1412 <2360808527@qq.com>
2023-08-19 16:09:53 +0800

    学成在线_添加代码生成器和生成po类

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/CoursePublishService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.content.model.po.CoursePublish;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程发布 服务类
+ * </p>
+ *
+ * @author 陈泽哲
+ * @since 2023-08-19
+ */
+public interface CoursePublishService extends IService<CoursePublish> {
+
+}
---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/TeachplanMediaService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.content.model.po.TeachplanMedia;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 陈泽哲
+ * @since 2023-08-19
+ */
+public interface TeachplanMediaService extends IService<TeachplanMedia> {
+
+}
