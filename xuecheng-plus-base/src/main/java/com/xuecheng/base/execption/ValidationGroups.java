66dda4e7af47dbabe598c28a831221e67f5b752a
陈泽哲 <2360808527@qq.com>
2023-08-21 10:59:47 +0800

    学成在线_添加新增课程接口，全局异常处理和自定义异常和参数校验

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/execption/ValidationGroups.java
@@ -0,0 +1,15 @@
+package com.xuecheng.base.execption;
+
+/**
+ * @author czz
+ * @version 1.0
+ * @description 校验分组
+ * @date 2022/9/8 15:05
+ */
+public class ValidationGroups {
+
+    public interface Inster{};
+    public interface Update{};
+    public interface Delete{};
+
+}
--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -1,12 +1,17 @@
 package com.xuecheng.content.api;
 
+import com.xuecheng.base.execption.ValidationGroups;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +34,14 @@ public class CourseBaseInfoController {
         log.info("接收到的分页参数为：{};查询条件为{}", pageParams, queryCourseParams);
         return courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParams);
 
+    }
+
+    @ApiOperation("新增课程基础信息")
+    @PostMapping("/course")
+    public CourseBaseInfoDto createCourseBase(@RequestBody @Validated({ValidationGroups.Inster.class}) AddCourseDto addCourseDto){
+        //机构id，由于认证系统没有上线暂时硬编码
+        Long companyId = 1232141425L;
+        return courseBaseInfoService.createCourseBase(companyId,addCourseDto);
     }
 
 }
